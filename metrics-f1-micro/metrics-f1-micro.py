93207444a088e73eaed89507240e9793ef691747
Rizwan Khan <104319753+Rkhan495@users.noreply.github.com>
2026-07-29 13:55:43 +0530

    Fix variable names in f1_micro function

--- metrics-f1-micro/metrics-f1-micro.py
+++ metrics-f1-micro/metrics-f1-micro.py
@@ -2,10 +2,10 @@ def f1_micro(y_true, y_pred) -> float:
     """
     Compute micro-averaged F1 for multi-class integer labels.
     """
-    tp, fpfn = 0, 0
+    tptn, fpfn = 0, 0
     for i in range(len(y_true)):
         if y_true[i] == y_pred[i] :
-            tp += 1
+            tptn += 1
         elif y_true[i] != y_pred[i]:
             fpfn += 1 
-    return (tp / (tp + (0.5 * (fpfn+fpfn))))
+    return (tptn / (tptn + (0.5 * (fpfn+fpfn))))
